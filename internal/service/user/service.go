3f08acfdd1615454a9ce0e9d693678c698d7a067
Vadim Filimonov <filimonov@duck.com>
2024-11-07 17:16:54 +0300

    Add login method; refactor login service layer

--- internal/service/user/service.go
+++ internal/service/user/service.go
@@ -1,25 +1,44 @@
 package user
 
 import (
+	"context"
+	"errors"
 	"fmt"
+	"time"
 
 	"golang.org/x/crypto/bcrypt"
 
 	"github.com/vadimfilimonov/house/internal/models"
 )
 
-type storage interface {
+const (
+	WrongPasswordErr = "password is wrong"
+)
+
+type userStorage interface {
 	Add(email, hashedPassword, userType string) (id *string, err error)
-	Get(email string) (*models.User, error)
+	Get(id string) (*models.User, error)
+}
+
+type tokenStorage interface {
+	Add(ctx context.Context, key, value string, expiration time.Duration) error
+}
+
+type tokenManager interface {
+	Encode(sub string) (*string, error)
 }
 
 type UserManager struct {
-	storage storage
+	userStorage  userStorage
+	tokenStorage tokenStorage
+	tokenManager tokenManager
 }
 
-func New(storage storage) *UserManager {
+func New(userStorage userStorage, tokenStorage tokenStorage, tokenManager tokenManager) *UserManager {
 	return &UserManager{
-		storage: storage,
+		userStorage:  userStorage,
+		tokenStorage: tokenStorage,
+		tokenManager: tokenManager,
 	}
 }
 
@@ -34,7 +53,7 @@ func (u *UserManager) Register(email, password, userType string) (*string, error
 		return nil, fmt.Errorf("cannot hash password: %w", err)
 	}
 
-	id, err := u.storage.Add(email, hash, userType)
+	id, err := u.userStorage.Add(email, hash, userType)
 	if err != nil {
 		return nil, fmt.Errorf("cannot save user in storage: %w", err)
 	}
@@ -42,8 +61,38 @@ func (u *UserManager) Register(email, password, userType string) (*string, error
 	return id, nil
 }
 
+func (u *UserManager) Login(ctx context.Context, id, password string) (*string, error) {
+	user, err := u.userStorage.Get(id)
+	if err != nil {
+		return nil, err
+	}
+
+	isPasswordCorrect := verifyPassword(password, user.Password)
+	if !isPasswordCorrect {
+		return nil, errors.New(WrongPasswordErr)
+	}
+
+	token, err := u.tokenManager.Encode(user.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	err = u.tokenStorage.Add(ctx, user.ID, *token, 24*time.Hour)
+	if err != nil {
+		return nil, err
+	}
+
+	return token, nil
+}
+
 // hashPassword generates a bcrypt hash for the given password.
 func hashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
 	return string(bytes), err
+}
+
+// VerifyPassword verifies if the given password matches the stored hash.
+func verifyPassword(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
