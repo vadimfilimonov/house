e194398b7c58c128f86471e9634d39145c71fa88
Vadim Filimonov <filimonov@duck.com>
2025-02-07 17:02:19 +0300

    Add comments to user model

--- internal/models/user.go
+++ internal/models/user.go
@@ -11,10 +11,10 @@ const (
 )
 
 type User struct {
-	ID       string `json:"user_id"   db:"user_id"   example:"123e4567-e89b-12d3-a456-426655440000"`
-	Email    string `json:"email"     db:"email"     example:"ivanov@mail.ru"`
-	Password string `json:"password"  db:"password"  example:"qwerty"`
-	UserType string `json:"user_type" db:"user_type" example:"client"`
+	ID       string `json:"user_id"   db:"user_id"   example:"123e4567-e89b-12d3-a456-426655440000"` // Идентификатор пользователя
+	Email    string `json:"email"     db:"email"     example:"ivanov@mail.ru"`                       // Email пользователя
+	Password string `json:"password"  db:"password"  example:"qwerty"`                               // Пароль пользователя
+	UserType string `json:"user_type" db:"user_type" example:"client"`                               // Тип пользователя
 }
 
 func (u *User) IsClient() bool {
