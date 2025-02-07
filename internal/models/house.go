ad3f22dd49f3300e13a2ef7ea2e4c312e83f1f57
Vadim Filimonov <filimonov@duck.com>
2025-02-07 14:37:35 +0300

    Comment each field in house model

--- internal/models/house.go
+++ internal/models/house.go
@@ -1,10 +1,10 @@
 package models
 
 type House struct {
-	ID        string  `json:"id"                   db:"id"`
-	Address   string  `json:"address"              db:"address"`
-	Year      int     `json:"year"                 db:"year"`
-	Developer *string `json:"developer,omitempty"  db:"developer"`
-	CreatedAt *string `json:"created_at,omitempty" db:"created_at"`
-	UpdateAt  *string `json:"update_at,omitempty"  db:"update_at"`
+	ID        string  `json:"id"                   db:"id"`         // Уникальный номер дома
+	Address   string  `json:"address"              db:"address"`    // Адрес
+	Year      int     `json:"year"                 db:"year"`       // Год постройки
+	Developer *string `json:"developer,omitempty"  db:"developer"`  // Застройщик
+	CreatedAt *string `json:"created_at,omitempty" db:"created_at"` // Дата создания дома в базе
+	UpdateAt  *string `json:"update_at,omitempty"  db:"update_at"`  // Дата последнего добавления новой квартиры дома
 }
