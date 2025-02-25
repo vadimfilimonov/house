14fb7763663da50d48bb275287a2488f2a57de7e
Vadim Filimonov <filimonov@duck.com>
2025-02-26 00:19:42 +0300

    Rename query var

--- internal/store/flat/flat.go
+++ internal/store/flat/flat.go
@@ -27,8 +27,8 @@ func (s *Store) Add(ctx context.Context, number, houseID, price, rooms int) (*mo
 	ctx, cancel := context.WithTimeout(ctx, defaultTimeout)
 	defer cancel()
 
-	query := `INSERT INTO flats (number, house_id, price, rooms, status) VALUES ($1, $2, $3, $4, $5)`
-	if _, err := s.storage.ExecContext(ctx, query, number, houseID, price, rooms, models.CreatedStatus); err != nil {
+	flatsQuery := `INSERT INTO flats (number, house_id, price, rooms, status) VALUES ($1, $2, $3, $4, $5)`
+	if _, err := s.storage.ExecContext(ctx, flatsQuery, number, houseID, price, rooms, models.CreatedStatus); err != nil {
 		return nil, fmt.Errorf("cannot add flat to database: %w", err)
 	}
 
